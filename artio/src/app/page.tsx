4902a01973a1c9e8a1aaa44338d1ab139b1d357e
Medamine-Bahassou <mohamedamine.bahassou@gmail.com>
2025-07-10 00:37:58 +0100

    add env

--- artio/src/app/page.tsx
+++ artio/src/app/page.tsx
@@ -88,10 +88,11 @@ const ImageGeneratorPage = () => {
         return;
       }
     }
+    const apiUrl = process.env.API_URL;
 
     try {
       // Replace with your actual API endpoint
-      const response = await fetch("http://127.0.0.1:5000/generate", {
+      const response = await fetch(`${apiUrl}/generate`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ prompt, width, height, num_outputs: numOutputs }),
